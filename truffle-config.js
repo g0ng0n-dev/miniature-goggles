4d6b1e26bf8d8741629083d80472f6e2e25a26ef
Gon Gis <g0ng0n.dev@gmail.com>
2022-01-03 12:32:51 +0100

    adding infura config

--- truffle-config.js
+++ truffle-config.js
@@ -18,6 +18,17 @@ module.exports = {
         return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", MetaMaskAccountIndex )
       },
       network_id: 5777,
+    },  ropsten_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/YOUR_INFURA_ID", MetaMaskAccountIndex)
+      },
+      network_id: 3
+    },
+    goerli_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/YOUR_INFURA_ID", MetaMaskAccountIndex)
+      },
+      network_id: 5
     }
   },
   compilers: {
