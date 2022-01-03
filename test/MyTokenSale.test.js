2a249bbd2016f14acb7b903a3a67006c5431bad6
Gon Gis <g0ng0n.dev@gmail.com>
2022-01-03 11:41:46 +0100

    adding KycContract contract

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -1,5 +1,7 @@
 const MyTokenSale = artifacts.require("MyTokenSale");
 const Token = artifacts.require("MyToken");
+const KycContract = artifacts.require("KycContract");
+
 const chai = require("./setupchai.js");
 require('dotenv').config({path: '../.env'});
 
@@ -12,6 +14,27 @@ contract("TokenSale Test", async(accounts) => {
     it("should not have any tokens in my deployer account", async() => {
         let instance = await Token.deployed();
         return expect(instance.balanceOf.call(initialHolder)).to.eventually.be.a.bignumber.equal(new BN(0));
+    });
+
+    it("all coins should be in the tokensale smart contract", async () => {
+        let instance = await Token.deployed();
+        let balance = await instance.balanceOf.call(MyTokenSale.address);
+        let totalSupply = await instance.totalSupply.call();
+        return expect(balance).to.be.a.bignumber.equal(totalSupply);
+    });
+    
+    it("should be possible to buy one token by simply sending ether to the smart contract", async () => {
+        let tokenInstance = await Token.deployed();
+        let tokenSaleInstance = await MyTokenSale.deployed();
+        let balanceBeforeAccount = await tokenInstance.balanceOf.call(recipient);
+        await expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1", "wei")})).to.be.rejected;
+        await expect(balanceBeforeAccount).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
+
+        let kycInstance = await KycContract.deployed();
+        await kycInstance.setKycCompleted(recipient);
+        await expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+        return expect(balanceBeforeAccount + 1).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
+    
     });
 
 } )
