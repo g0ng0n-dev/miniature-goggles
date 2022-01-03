16698dc1b9d4c35d9049ef2eb4e4cfb46652090f
Gon Gis <g0ng0n.dev@gmail.com>
2022-01-03 10:59:21 +0100

    adding new contract, test and deploy for that contract, also adding dotenv dependency

---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.6.0;
+
+import "./Crowdsale.sol";
+
+contract MyTokenSale is Crowdsale {
+
+    //KycContract kyc;
+    constructor(
+        uint256 rate, //Rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+        {
+
+        }
+}
--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,4 +1,5 @@
 const Token = artifacts.require("MyToken");
+require("dotenv").config({path: "../.env"});
 
 var chai = require("chai");
 const BN = web3.utils.BN;
@@ -12,18 +13,22 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract("Token Test", async(accounts) => {
-
     const[deployerAccount, recipient, anotherAccount ] = accounts;
 
+    beforeEach(async() => {
+        this.myToken = await Token.new(process.env.INITIAL_TOKENS);
+    })
+
+
     it("all tokens hould be in my account", async () => {
-        let instance = await Token.deployed();
+        let instance = await this.myToken;
         let totalSupply = await instance.totalSupply();
         await expect(instance.balanceOf(deployerAccount)).to.eventually.be.bignumber.equal(totalSupply)
     });
 
     it("is possible to sned tokends between accounts", async() => {
         const sendTokens = 1;
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
         await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
         await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
@@ -32,7 +37,7 @@ contract("Token Test", async(accounts) => {
     });
 
     it("It's not possible to send more tokens than account 1 has", async () => {
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let balanceOfDeployer = await instance.balanceOf(deployerAccount);
 
         await expect(instance.transfer(recipient, new BN(balanceOfDeployer+1))).to.eventually.be.rejected;
